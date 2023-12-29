dd811b8a18f9c71e5ad84dd1f29d80b4069e790c
Guillaume <pastres.guillaume@gmail.com>
2023-12-29 22:15:55 +0100

    Create Models and ValueObjects

---
+++ JeBalance.Domain/Models/Crime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeBalance.Domain.Models
+{
+    public enum Crime
+    {
+        None = 0,
+        DissimulationDeRevenus = 1,
+        EvasionFiscale = 2
+    }
+}
---
+++ JeBalance.Domain/Models/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using JeBalance.Domain.Contracts;
+using JeBalance.Domain.ValueObjects;
+
+namespace JeBalance.Domain.Models
+{
+    public class Person : Entity
+    {
+        public Name FirstName { get; }
+        public Name LastName { get; }
+        public Address Address { get; }
+        public bool IsBanned { get; }
+
+        public Person() : base(0)
+        {
+        }
+
+        public Person(int id, Name firstName, Name lastName, Address address, bool isBanned) : base(id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            IsBanned = isBanned;
+        }
+
+        public Person(int id, string firstName, string lastName, Address address, bool isBanned) : this(
+            id,
+            new Name(firstName),
+            new Name(lastName),
+            address,
+            isBanned
+            ) 
+        { }
+
+        public Person(int id, string firstName, string lastName, Address address) : this(
+            id,
+            new Name(firstName),
+            new Name(lastName),
+            address,
+            false
+            )
+        { }
+
+    }
+}
