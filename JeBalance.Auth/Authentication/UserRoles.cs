bb956e68ca3ef0e9de215899a452cb106fb709e1
Max <max.aumard93@gmail.com>
2024-01-23 01:19:45 +0100

    Authentification project

---
+++ JeBalance.Auth/Authentication/UserRoles.cs
@@ -0,0 +1,8 @@
+
+namespace JeBalance.Auth.Authentication;
+public class UserRoles
+{
+    public const string Admin = "Admin";
+    public const string User = "User";
+    public const string Inspector = "Inspector";
+}
